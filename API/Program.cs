9b2d314d7e3da054466dcb8b6fcd8191af271e59
Emma Gummeson <emma.gummeson@elev.ga.ntig.se>
2021-12-06 13:12:37 +0100

    first and last

---
+++ API/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RestSharp;
+using System.Text.Json;
+
+RestClient pokeClient = new RestClient("https://pokeapi.co/api/v2/");
+RestRequest pokeRequest = new RestRequest("pokemon/snorlax");
+IRestResponse pokeResponse = pokeClient.Get(pokeRequest);
+
+Console.WriteLine(pokeResponse.StatusCode);
+Console.WriteLine(pokeResponse.Content);
+
+Pokemon p = JsonSerializer.Deserialize<Pokemon>(pokeResponse.Content);
+
+Console.WriteLine(p.name);
+
+//starwars
+//RestClient swClient = new RestClient("https://swapi.dev/api/");
+//RestRequest swRequest = new RestRequest("people/1");
+//IRestResponse swResponse = swClient.Get(swRequest);
+
+//Console.WriteLine(swResponse.Content);
+
+//Person p = JsonSerializer.Deserialize<Person>(swResponse.Content);
+
+//Console.WriteLine(p.name);
+
+Console.ReadLine();
